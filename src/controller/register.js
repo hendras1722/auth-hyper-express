45d20572af17bac04e1d02b9674d8a041dd483bf
hendras1722 <muhsyahendraa1722@gmail.com>
2025-06-08 08:22:27 +0700

    fix: validate email uniqueness before registration and improve response structure

--- src/controller/register.js
+++ src/controller/register.js
@@ -14,13 +14,20 @@ async function Register(req, res) {
   try {
     const db = await connectDB()
     const { email, password } = req.body
+    const checkEmail = await db.collection('users').findOne({ email })
+    if (checkEmail)
+      return res.status(400).json({
+        code: 400,
+        message: 'Email already exists',
+      })
     await validationZod(schemaRegister, req.body)
     const HashPassword = await hashingHmac(password)
-    await db.collection('users').insertOne(req.body)
+    await db.collection('users').insertOne({ email, password: HashPassword })
+    const result = await db.collection('users').findOne({ email })
     return res.status(200).json({
       code: 200,
       message: 'Success',
-      data: { email, password: HashPassword },
+      data: { id: result.insertedId, email },
     })
   } catch (error) {
     console.log(error.message)
