0177387b3be920be726eeac3fb2bb345bc6b887b
hendras1722 <muhsyahendraa1722@gmail.com>
2025-06-08 16:26:26 +0700

    fix: update parameter names in JWT token generation functions for clarity

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
-function generateAccessToken(username) {
-  return jwt.sign(username, process.env.JWT_KEY, { expiresIn: '1800s' })
+function generateAccessToken(payload) {
+  return jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1800s' })
 }
 
-function generateRefreshToken(username) {
-  return jwt.sign(username, process.env.JWT_SECRET_REFRESH, {
+function generateRefreshToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET_REFRESH, {
     expiresIn: '7d',
   })
 }
