53f092e72279a6390473f63b4935995f5cc7df3e
hendras1722 <muhsyahendraa1722@gmail.com>
2025-06-08 08:06:43 +0700

    refactor: remove serverless handler and streamline MongoDB connection middleware

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const Routes = require('./src/routes')
 const cookieParser = require('cookie-parser')
 const { connectDB } = require('./src/configs/mongodb')
-const serverless = require('serverless-http')
 
 const app = express()
 
@@ -29,27 +28,25 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' })
 })
 
-const handler = serverless(app)
+// MongoDB connect middleware
+app.use(async (req, res, next) => {
+  try {
+    await connectDB()
+    next()
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'MongoDB connect failed' })
+  }
+})
 
-// Ekspor handler untuk serverless
-module.exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false
-
-  // Pastikan MongoDB connect (1x reuse)
-  await connectDB()
-
-  return handler(event, context)
-}
-
-// Kalau mau running lokal (misalnya: node index.js)
 if (require.main === module) {
   const port = process.env.PORT || 3000
-  connectDB()
-    .then(() => {
-      console.log('MongoDB is ready')
-      app.listen(port, () => {
-        console.log(`Server listening on port ${port}`)
-      })
+  connectDB().then(() => {
+    console.log('MongoDB is ready')
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`)
     })
-    .catch(console.error)
+  })
 }
+
+module.exports = app
