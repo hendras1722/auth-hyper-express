e252cbf0d37649fc2f0deedfb627c3734750adcd
hendras1722 <muhsyahendraa1722@gmail.com>
2025-06-14 14:06:27 +0700

    fix: refactor MongoDB connection middleware to improve error handling and code organization

--- index.js
+++ index.js
@@ -13,11 +13,24 @@ const path = require('path')
 const app = express()
 
 app
+  .use(async (req, res, next) => {
+    try {
+      await connectDB()
+      next()
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ message: 'MongoDB connect failed' })
+    }
+  })
   .use(cors())
   .use(express.json())
   .use(cookieParser())
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.urlencoded({ extended: true }))
+  .use('/v1', Routes)
+  .get('/', (req, res) => {
+    res.send('ping')
+  })
   .use(
     '/docs',
     swaggerUI.serve,
@@ -44,25 +57,12 @@ app
       },
     })
   )
-  .use('/v1', Routes)
-  .get('/', (req, res) => {
-    res.send('ping')
-  })
   .use((req, res, next) => {
     StatusError(res, 404, 'Route not found', 'Oh You are lost')
   })
   .use((err, req, res, next) => {
     console.error(err)
     StatusError(res, 500, 'Internal Server Error', 'Something wrong!')
-  })
-  .use(async (req, res, next) => {
-    try {
-      await connectDB()
-      next()
-    } catch (err) {
-      console.error(err)
-      res.status(500).json({ message: 'MongoDB connect failed' })
-    }
   })
 
 if (require.main === module) {
